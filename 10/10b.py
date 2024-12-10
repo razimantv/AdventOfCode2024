315850f588b5c67aa9dbd8ae239b4d7c6cfa7fec
Raziman T V <razimantv@gmail.com>
2024-12-10 09:07:48 +0000

    Q10

---
+++ 10/10b.py
@@ -0,0 +1,24 @@
+from functools import cache
+
+with open('10.in') as file:
+    grid = [line.strip() for line in file]
+
+m, n = len(grid), len(grid[0])
+
+@cache
+def dp(i, j):
+    if grid[i][j] == '9':
+        return 1
+    ret = 0
+    for di, dj in [(0,1), (1,0), (0,-1), (-1,0)]:
+        ni, nj = i + di, j + dj
+        if (
+            0 <= ni < m and 0 <= nj < n
+        ) and int(grid[ni][nj]) == int(grid[i][j]) + 1:
+            ret += dp(ni, nj)
+    return ret
+
+print(sum(
+    dp(i, j) for i in range(m) for j in range(n)
+    if grid[i][j] == '0'
+))
