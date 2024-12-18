255a270db55f42c7a0d45e8abbcd0db5a8dd3b18
Raziman T V <razimantv@gmail.com>
2024-12-18 14:04:13 +0000

    Q18

---
+++ 18/18b.py
@@ -0,0 +1,33 @@
+L = 71
+dr = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+with open('18.in') as file:
+    bad = [tuple(map(int, line.strip().split(','))) for line in file]
+
+
+def dist(n):
+    grid = [[-1] * L for _ in range(L)]
+    for i, j in bad[:n]:
+        grid[i][j] = -2
+
+    grid[0][0] = 0
+    todo = [(0, 0)]
+    while todo:
+        next = []
+        for i, j in todo:
+            for di, dj in dr:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < L and 0 <= nj < L and grid[ni][nj] == -1:
+                    grid[ni][nj] = grid[i][j] + 1
+                    next.append((ni, nj))
+        todo = next
+    return grid[-1][-1]
+
+
+start, end = 0, len(bad) + 1
+while end - start > 1:
+    mid = (start + end) // 2
+    if dist(mid) > -1:
+        start = mid
+    else:
+        end = mid
+print(bad[start])
