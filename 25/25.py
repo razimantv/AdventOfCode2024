9b6bb79d763e6213a87b0d13239ba5d1ef0271a9
Raziman T V <razimantv@gmail.com>
2024-12-25 21:09:15 +0000

    Readme for Q25

--- 25/25.py
+++ 25/25.py
@@ -1,19 +1,15 @@
-def process(grid):
-    keylock = 0 if grid[0][0] == '#' else 1
-    return keylock, [
-        sum(1 for i in range(1, 6) if grid[i][j] == '#')
-        for j in range(5)
-    ]
+from functools import reduce
+from operator import or_
+from itertools import batched, combinations
+
+
+def mask(grid):
+    return reduce(or_, (1 << i for i, c in enumerate(grid) if c == '#'), 0)
 
 
 with open('25.in') as file:
-    input = [line.strip() for line in file]
-
-patterns, ret = [[], []], 0
-for i in range(0, len(input), 8):
-    keylock, pattern = process(input[i:i + 7])
-    patterns[keylock].append(pattern)
-    for other in patterns[1 - keylock]:
-        if all(p + q <= 5 for p, q in zip(pattern, other)):
-            ret += 1
-print(ret)
+    masks = [
+        mask(''.join(line.strip() for line in grid))
+        for grid in batched(file, 8)
+    ]
+print(sum(1 for x, y in combinations(masks, 2) if x & y == 0))
---
+++ 25/readme.md
@@ -0,0 +1,5 @@
+# Day 25
+
+[Problem link](https://adventofcode.com/2024/day/25)
+
+If we turn each grid into a bitmask with 1 for #, valid key-lock pairs are exactly those mask pairs with a bitwise AND of 0.
